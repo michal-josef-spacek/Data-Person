f9c0f6891df321c997ebfa4c9ae428adf491cdd8
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-26 13:57:13 +0200

    Prepare for 0.06 version

--- Person.pm
+++ Person.pm
@@ -11,7 +11,7 @@ use Readonly;
 
 Readonly::Array our @SEX => qw(female male unknown);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 has email => (
 	is => 'ro',
@@ -254,6 +254,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
